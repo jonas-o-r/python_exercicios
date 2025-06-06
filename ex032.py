976504284ec46e7dd9470f2460bf3113383febfa
jonas-o-r <jonasorezende@gmail.com>
2025-06-05 22:19:05 -0300

    Manip. de texto OK

--- ex032.py
+++ ex032.py
@@ -1,6 +1,6 @@
 #Faça um programa que leia um ano qualquer e mostre se ele é bissexto.
 ano = int(input('Informe um ano: '))
-if (ano / 4) == (ano // 4) and (ano / 400) == (ano // 400):
+if (ano % 4) == 0 and (ano % 400) == 0 or (ano % 100) != 0:
     print('O ano é bissexto.')
 else:
     print('O ano não é bissexto.')

b6b99a8fea5cf96e23917cc68e98da8210690ca4
Jonas <jonasorezende@gmail.com>
2025-06-06 17:31:27 -0300

    Exercícios atualizados

--- ex033.py
+++ ex033.py
@@ -1,7 +1,16 @@
 #Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
-n1 = float(input('Informe o 1ª número: '))
-n2 = float(input('Informe o 2ª número: '))
-n3 = float(input('Informe o 3ª número: '))
-lst = [n1, n2, n3]
-print(f'Maior número: {max(lst)}')
-print(f'Menor número: {min(lst)}')
+a = float(input('Informe o 1ª número: '))
+b = float(input('Informe o 2ª número: '))
+c = float(input('Informe o 3ª número: '))
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print(f'Maior número: {maior}')
+print(f'Menor número: {menor}')
